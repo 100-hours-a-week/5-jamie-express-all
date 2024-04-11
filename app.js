7005832edb5f8e1a7c5b40a197209c9473e2d3ed
yoouung <jyp101400@egmail.com>
2024-04-11 11:21:17 +0900

    add EXPRESS server
    - add node & express server
    - refactoring
    
    Signed-off-by: yoouung <jyp101400@egmail.com>

---
+++ app.js
@@ -0,0 +1,39 @@
+// import express module
+import express from "express";
+import path from "path";
+
+// create express app
+const app = express();
+// define port number
+const port = 3000;
+// ES module에서는 __dirname 사용 불가
+const __dirname = path.resolve();
+
+/* req = request, res = response */
+
+/**
+ * 정적 파일 제공을 위한 디렉토리 설정
+ * - express.static 미들웨어 가 정적 파일을 제공한다.
+ * */
+app.use(express.static("public"));
+
+const pages = [
+    { route: "/", filePath: "index.html" },
+    { route: "/signup", filePath: "user/signup.html" },
+    { route: "/update-info", filePath: "user/updateInfo.html" },
+    { route: "/update-password", filePath: "user/updatePwd.html" },
+    { route: "/post-list", filePath: "post/list.html" },
+    { route: "/post-write", filePath: "post/write.html" },
+    { route: "/post-detail", filePath: "post/contents.html" },
+    { route: "/post-update", filePath: "post/update.html" },
+];
+
+pages.forEach((page) => {
+    app.get(page.route, (req, res) => {
+        res.sendFile(path.join(__dirname, "public/html", page.filePath));
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
